c03624599e08dc2b0c36439f99dce77066f03208
kielhong <kielhong@sk.com>
2014-11-11 11:22:20 +0900

    Reslove #5

--- cafe-sectionweb/src/main/java/net/kiel/cafe/web/controller/IndexController.java
+++ cafe-sectionweb/src/main/java/net/kiel/cafe/web/controller/IndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-public class CafeController {
+public class IndexController {
     
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(Model model) {
