2eaf83004143c563c39d3b0385172f89321e413f
kielhong <kielhong@sk.com>
2015-04-03 17:52:26 +0900

    #21  @EnaleWebMvcSecurity -> @EnableWebSecurity  로 변경 (deprecated)

--- cafe-sectionweb/src/main/java/net/kiel/cafe/web/config/SecurityConfig.java
+++ cafe-sectionweb/src/main/java/net/kiel/cafe/web/config/SecurityConfig.java
@@ -8,12 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Configuration
-@EnableWebMvcSecurity
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Autowired
     DataSource dataSource;
@@ -45,9 +44,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
                 .and()
             .csrf().disable();
     }
-    
-//    @Override
-//    protected UserDetailsService userDetailsService() {
-//        return userService;
-//    }
 }
